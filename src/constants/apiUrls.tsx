d6d4045b39c42ae8d4f7316eb977cab5beb56220
Tugran Demirel <demireltugran66@gmail.com>
2025-01-10 01:17:38 +0300

    #23 tab menü içerisine haberleri getirme işlemi

--- src/constants/apiUrls.tsx
+++ src/constants/apiUrls.tsx
@@ -2,7 +2,7 @@ const API_URLS = {
     CATEGORIES:         '/categories', // Kategorilere ait API URL'si
     LAST_MINUTES:       '/last-minutes', // Son Dakika ait API URL'si
     FEATURED_NEWS:      '/featured-news', // Öne Çıkanlara ait API URL'si
-    CATEGORY_NEWSLETTERS: "/category-newsletters"
+    CATEGORY_NEWSLETTERS: "/category-newsletters" // Tab Menü için Kategori Bazlı Haber
 };
 
 export default API_URLS;
